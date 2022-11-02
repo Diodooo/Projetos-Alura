dc6c6dab2fc92100b79a85759ec9136c392e3d60
Diodooo <110136149+Diodooo@users.noreply.github.com>
2022-11-02 18:10:19 -0300

    Add files via upload

---
+++ ByteBank/ByteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,77 @@
+﻿using ByteBank.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Contas
+{
+    public class ContaCorrente
+    {
+        public static int ContasCriadas { get; private set; }
+        private short numeroAgencia;
+        public short NumeroAgenica {
+            private set {
+                if ( value > 0 ) {
+                    this.numeroAgencia = value; 
+                }
+            }
+            get { return this.numeroAgencia; }
+        }
+
+        public string Conta { get; set; }
+        private double saldo;
+        public Cliente Titular { get; set; }
+      
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+            Console.WriteLine("Ação de depósito realizada --> Valor: " + valor + ", Saldo atual:" + saldo);
+        }
+        public void Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                Console.WriteLine("Ação de saque realizada --> Valor: " + valor + ", Saldo atual:" + saldo);
+                return;
+            }
+            Console.WriteLine("Saldo insuficiente para executar esta operação");
+        }
+
+        public void Transferencia(double valor, ContaCorrente destino)
+        {
+            Sacar(valor);
+            destino.Depositar(valor);
+        }
+
+        public void SetSaldo(double valor) {
+            if(valor > 0) {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo() {
+            return this.saldo;
+        }
+
+        public ContaCorrente(string Conta, short numeroAgencia) {
+            this.Conta = Conta;
+            this.NumeroAgenica = numeroAgencia;
+            this.Titular = new Cliente();
+            ContasCriadas++;
+        }
+
+        public void GetInformacao () {
+            Console.WriteLine("- Nome: " + this.Titular.Nome);
+            Console.WriteLine("- CPF: " + this.Titular.Cpf);
+            Console.WriteLine("- Profissão: " + this.Titular.Profissao);
+            Console.WriteLine("- Numero da Agencia: " + this.NumeroAgenica);
+            Console.WriteLine("- Conta: " + this.Conta);
+            Console.WriteLine("- Saldo: " + this.GetSaldo());
+        }
+
+
+    }
+}
---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using ByteBank.Clientes;
+using ByteBank.Contas;
+
+Console.WriteLine("Bem vindo ao programa de cadastro do banco ByteBank!");
+Console.Write("Informe seu nome: ");
+string nome = Console.ReadLine();
+ContaCorrente conta = new ContaCorrente("1245-X", 12);
+conta.Titular = new Cliente();
+conta.Titular.Nome = nome;
+Console.WriteLine("Muito bem " + conta.Titular.Nome + ", agora nos informe seu CPF e em seguida sua profissão: ");
+Console.Write("CPF: ");
+string cpf = Console.ReadLine();
+conta.Titular.Cpf = cpf;
+Console.Write("Profissão: ");
+string profissao = Console.ReadLine();
+conta.Titular.Profissao = profissao;
+Console.WriteLine("Aguarde enquanto fazemos mais algumas configurações...");
+//Nenhuma configuração está sendo feita :)
+Thread.Sleep(1500);
+Console.WriteLine(@"Gostaria de depositar um valor inicial?
+- 0 = Não
+- 1 = Sim");
+string valInicial = Console.ReadLine();
+if (valInicial == "1") {
+    Console.Write("Informe o valor: ");
+    double valor = double.Parse(Console.ReadLine());
+    conta.SetSaldo(valor);
+} else {
+    Console.WriteLine("Tudo bem então!");
+}
+Console.WriteLine("Conta Finalizada!");
+Console.WriteLine("Abaixo estão suas informações: ");
+conta.GetInformacao();
+
+Console.WriteLine("Pressione ENTER para sair...");
+Console.ReadLine();
+
+//ContaCorrente contaDoAndre = new ContaCorrente("1245-X", 12);
+//contaDoAndre.Titular = new Cliente();
+//contaDoAndre.Titular.Profissao = "Marceneiro";
+//Console.WriteLine(ContaCorrente.ContasCriadas);
+
+
+
+
+
+
+
